d4513500ca245f80438770403d6fb2b944c14016
Malik <malik43213@gmail.com>
2020-08-31 13:14:49 +0200

    commit

--- Database/Database.php
+++ Database/Database.php
@@ -2,10 +2,10 @@
 class Database
 {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'ChefMeDb';
-    private $username = 'root';
-    private $password = 'root';
+    private $host = 'eu-cdbr-west-03.cleardb.net';
+    private $db_name = 'heroku_2f403f9964068f2';
+    private $username = 'b16742d66ff8f3';
+    private $password = 'ec3fd829';
     private $conn;
 
     // DB Connect
