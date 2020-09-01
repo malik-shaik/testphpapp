17441d7f4217f2f685841559c0711d0151d7f3bb
Malik <malik43213@gmail.com>
2020-09-01 13:35:14 +0200

    commit

--- Database/Database.php
+++ Database/Database.php
@@ -2,10 +2,15 @@
 class Database
 {
     // DB Params
-    private $host = 'eu-cdbr-west-03.cleardb.net';
-    private $db_name = 'heroku_2f403f9964068f2';
-    private $username = 'b16742d66ff8f3';
-    private $password = 'ec3fd829';
+    // private $host = 'eu-cdbr-west-03.cleardb.net';
+    // private $db_name = 'heroku_2f403f9964068f2';
+    // private $username = 'b16742d66ff8f3';
+    // private $password = 'ec3fd829';
+
+    private $host = $_ENV['HOST'];
+    private $db_name = $_ENV['DB_NAME'];
+    private $username = $_ENV['DB_USER'];
+    private $password = $_ENV['DB_PASS'];
     private $conn;
 
     // DB Connect
